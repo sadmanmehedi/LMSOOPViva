940e22230956e610e16f319bf22eb248ad2fb3f0
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-06-09 18:36:22 +0600

    Working with their Methods

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+librarians = []
+books = []
+users = []
+
+#...........................................................................
 class Person():
     def __init__(self,username):
         self.username=username
 
 #============================================================================
 class Admin(Person):
-    pass
+  def seeLibrarianList(self):
+      pass
+  def addLibrarian(self):
+      pass
+  def removeLibrarian(self):
+      pass
+  def seeBookList(self):
+      pass
+  def addBook(self):
+      pass
+  def removeBook(self):
+      pass
+
+
 
 
 #==============================================================================
 
+
 class Librarian(Person):
-    pass
+  def changePassword(self):
+      pass
+
+
 # ==============================================================================
 class User(Person):
     pass
